1b8200ed0dae68024a9dc2e0256850288828a6a9
azzireluto <bruno.a@outlook.com.br>
2022-05-27 15:25:26 -0300

    adding example of provider

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,47 @@
+import 'package:azzi_shop/providers/counter.dart';
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = CounterProvider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Exemplo Contador"),
+      ),
+      body: Column(
+        children: [
+          Text(provider?.state.value.toString() ?? "0"),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.inc();
+              });
+              print(provider?.state.value.toString());
+            },
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.dec();
+              });
+              print(provider?.state.value.toString());
+            },
+            icon: Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
